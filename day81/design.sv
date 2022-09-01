c8cd95d2739d17bcf50674cdfc6f194faa316722
Rahul Behl <raulbehl@gmail.com>
2022-09-01 08:20:30 +0530

    Day81 - Register File

---
+++ day81/design.sv
@@ -0,0 +1,12 @@
+`include "riscv_fetch.sv"
+`include "riscv_decode.sv"
+`include "riscv_regfile.sv"
+
+module riscv_top (
+  input		wire		clk,
+  input		wire		reset
+);
+  
+  // Instantiate and connect all the submodules
+  
+endmodule

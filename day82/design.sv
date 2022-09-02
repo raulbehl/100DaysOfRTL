fcb6406a08ca2868cac58688759de83802202312
Rahul Behl <raulbehl@gmail.com>
2022-09-02 19:03:55 +0530

    Day82 - RISCV Execute

---
+++ day82/design.sv
@@ -0,0 +1,13 @@
+`include "riscv_fetch.sv"
+`include "riscv_decode.sv"
+`include "riscv_regfile.sv"
+`include "riscv_execute.sv"
+
+module riscv_top (
+  input		wire		clk,
+  input		wire		reset
+);
+  
+  // Instantiate and connect all the submodules
+  
+endmodule
